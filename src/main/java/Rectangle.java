8e4ec3136302b6c1b63ca4c581fa0ef01aa04d88
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-16 17:53:41 +0100

    add calulations for rectange

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,13 +1,34 @@
 public class Rectangle {
 
-    double length;
-    double width;
+    float length;
+    float width;
 
-    public Rectangle(double length) {
+    public Rectangle(float length) {
         this.length = length;
         this.width = width;
     }
 
     // Rectangles Method
 
+
+    public float getLength() {
+        return length;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getPerimeter() {
+        return ((length + width) * 2);
+    }
+    
 }
