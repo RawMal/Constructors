cf7a02c1c0681d38d05330177d52fea9dc1e6631
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-16 23:35:55 +0100

    cust getters and setters

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,18 @@
 public class Customer {
+
+    int id;
+    String firstName;
+    String lastName;
+    double heldMoney;
+
+    public Customer(int id, String firstName, String lastName, double heldMoney) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.heldMoney = heldMoney;
+    }
+
+    // Customer getters + setters
+    
+
 }
