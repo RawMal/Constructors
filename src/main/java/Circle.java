f0e6c3c7575b9a6af6838777b6b0b11a2cd95e71
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-16 17:08:37 +0100

    Over ride the circle

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,2 +1,23 @@
 public class Circle {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    //Circles Method
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double newRadius) {
+        this.radius = newRadius;
+    }
+
+    public double getCircumference() {
+        return (2 * radius * Math.PI);
+    }
+
 }
