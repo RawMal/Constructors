9503d3c7fc2b784adce05865ea06b0dff6dbef7a
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-17 00:32:39 +0100

    cust main creation

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Customer {
 
     int id;
@@ -5,14 +7,57 @@ public class Customer {
     String lastName;
     double heldMoney;
 
+    Random random = new Random();
+
     public Customer(int id, String firstName, String lastName, double heldMoney) {
-        this.id = id;
+        this.id = (int) random.nextInt(500);
         this.firstName = firstName;
         this.lastName = lastName;
         this.heldMoney = heldMoney;
     }
 
     // Customer getters + setters
-    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getfirstName() {
+        return firstName = firstName;
+    }
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getlastName() {
+        return lastName = lastName;
+    }
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public double getHeldMoney()  {
+     return heldMoney = heldMoney;
+    }
+    public void setHeldMoney(double heldMoney) {
+        this.heldMoney = heldMoney;
+    }
+
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName +
+                ", lastName='" + lastName +
+                ", heldMoney=" + heldMoney +
+                '}';
+    }
+
+
+
 
 }
